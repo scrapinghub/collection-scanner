74f1fab73226095ac6aba833e8cf7eb2a4a70b50
Martin Olveyra <molveyra@gmail.com>
2017-01-20 17:19:10 -0300

    bump version 0.1.4->0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collection_scanner',
-    version='0.1.3',
+    version='0.1.5',
     packages=find_packages(),
     install_requires = [
         'dateparser',
