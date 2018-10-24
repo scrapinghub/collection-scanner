fd6581dfb2fbf62b37028d71684e5fd12aafcf92
Martin Olveyra <molveyra@gmail.com>
2018-10-24 14:44:00 -0300

    update setup.py for pypi release

--- setup.py
+++ setup.py
@@ -3,10 +3,22 @@ from setuptools import setup, find_packages
 setup(
     name='collection_scanner',
     version='0.2',
+    description='Scrapinghub Hubstorage Collection scanner.',
+    long_description = open('README.rst').read(),
+    license='BSD',
+    url= 'https://github.com/scrapinghub/collection-scanner',
+    maintainer='Scrapinghub',
     packages=find_packages(),
     install_requires = [
         'dateparser',
         'retrying',
         'scrapinghub>=2.0.3',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
     ]
 )
