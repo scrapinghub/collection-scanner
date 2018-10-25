724549195af0cbb2b1e0be691194b63d935212c1
Martin Olveyra <molveyra@gmail.com>
2018-10-25 12:03:34 -0300

    updated to version 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collection_scanner',
-    version='0.2.1',
+    version='0.3',
     description='Scrapinghub Hubstorage Collection scanner.',
     long_description = open('README.rst').read(),
     license='BSD',
