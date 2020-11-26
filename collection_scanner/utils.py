921307a5ef710bef3c56ca402bcb33d933289fa6
Martin Olveyra <molveyra@gmail.com>
2020-11-26 15:21:32 -0300

    added method for easy conversion of bytes comming from collection

--- collection_scanner/utils.py
+++ collection_scanner/utils.py
@@ -1,6 +1,7 @@
 import re
 import os
 import traceback
+import collections.abc
 
 
 LIMIT_KEY_CHAR = b'~'
@@ -51,3 +52,22 @@ def get_project_id():
         return os.environ['SHUB_JOBKEY'].split('/')[0]
     except KeyError:
         raise ValueError("Project id not found")
+
+
+def convert_bytes(obj):
+    """
+    >>> d = {b'saa': 5, 't': 8}
+    >>> convert_bytes(d)
+    {'saa': 5, 't': 8}
+    >>> convert_bytes(list(d.items()))
+    [('saa', 5), ('t', 8)]
+    """
+    if isinstance(obj, str):
+        return obj
+    if isinstance(obj, bytes):
+        return obj.decode()
+    if isinstance(obj, collections.abc.Mapping):
+        return dict(map(convert_bytes, obj.items()))
+    if isinstance(obj, collections.abc.Iterable):
+        return type(obj)(map(convert_bytes, obj))
+    return obj
